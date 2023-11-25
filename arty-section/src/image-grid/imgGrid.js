67506104ab46263ed2bfd8c26714bb8ab4211a25
ValentinAr7 <valio990210@gmail.com>
2023-11-25 13:17:12 +0200

    [Arty] Instagram image and fonts

--- arty-section/src/image-grid/imgGrid.js
+++ arty-section/src/image-grid/imgGrid.js
@@ -4,21 +4,37 @@ import image3 from "../assets/img3.png";
 import image4 from "../assets/img1.png";
 import image5 from "../assets/img1.png";
 import image6 from "../assets/img4.png";
+import instagramImage from "../assets/ig.PNG";
 import styles from "./imgGrid.module.css";
+import { StyledH2, StyledParagraph } from "../reusable";
 
 const images = [image1, image2, image3, image4, image5, image6];
 
 const ImgGrid = () => {
   return (
-    <div className={styles.gridContainer}>
-      {images.map((image, index) => (
+    <div>
+      <div className={styles.gridContainer}>
+        <div className={styles.headingSection}>
+          <StyledH2>Follow us on Instagram</StyledH2>
+          <StyledParagraph>tattoo_arty_studio</StyledParagraph>
+          <StyledParagraph>tattoo_arty_studio</StyledParagraph>
+          <StyledParagraph>tattoo_arty_studio</StyledParagraph>
+        </div>
+        {images.map((image, index) => (
+          <img
+            className={styles.image}
+            key={index}
+            src={image}
+            alt={`Image ${index + 1}`}
+          />
+        ))}
+
         <img
-          className={styles.image}
-          key={index}
-          src={image}
-          alt={`Image ${index + 1}`}
+          className={styles.instagramImg}
+          src={instagramImage}
+          alt="Instagram Image"
         />
-      ))}
+      </div>
     </div>
   );
 };
--- arty-section/src/reusable.js
+++ arty-section/src/reusable.js
@@ -1,2 +1,14 @@
+import styled from "styled-components";
 
+const StyledH2 = styled.h2`
+  font-family: "Pacifico", cursive;
+  left: 0;
+  margin: 10px;
+`;
 
+const StyledParagraph = styled.p`
+  font-family: "Arimo", sans-serif;
+  /* Add other styles as needed */
+`;
+
+export { StyledH2, StyledParagraph };
